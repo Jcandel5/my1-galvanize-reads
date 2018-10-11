4ad3ed371ecdcc1bd4fb5efd2d34695fcce18a5c
Jared Candelaria <jaredcandelaria@Jareds-MacBook-Pro.local>
2018-10-10 18:18:26 -0600

    front end working

--- queries.js
+++ queries.js
@@ -3,8 +3,9 @@ const knex = require('knex')(connection)
 
 
 function listAllData() {
-    return knex('authors_books')
+    return knex('authors_books').innerJoin('authors', 'authors_books.authors_id', 'authors.id').innerJoin('books', 'authors_books.books_id', 'books.id')
 }
+
 module.exports = {
     listAllData
 }
